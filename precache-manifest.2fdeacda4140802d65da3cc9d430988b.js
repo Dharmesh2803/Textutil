44f5055d25db2a5b5f42faa690294aacfebaad13
Dharmesh2803 <100507780+Dharmesh2803@users.noreply.github.com>
2024-01-03 16:15:59 +0530

    Updates

---
+++ precache-manifest.2fdeacda4140802d65da3cc9d430988b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ca43c0bc9b97158f19e662a27238dea",
+    "url": "/Textutil/index.html"
+  },
+  {
+    "revision": "121a60ece77059b19c32",
+    "url": "/Textutil/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/Textutil/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "e2a960147ef5362ced8a",
+    "url": "/Textutil/static/js/3.1b3c587a.chunk.js"
+  },
+  {
+    "revision": "121a60ece77059b19c32",
+    "url": "/Textutil/static/js/main.2b9ab3e1.chunk.js"
+  },
+  {
+    "revision": "c7aabde3f3e6e2a9ef84",
+    "url": "/Textutil/static/js/runtime~main.e683c8e8.js"
+  }
+]);
